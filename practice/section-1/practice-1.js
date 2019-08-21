40490e2f387fb9a9d23007f8c3445f90f07b00e8
yangjingsheng <673630886@qq.com>
2019-08-21 09:54:41 +0800

    add

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,10 @@
 'use strict';
 
 function find(collection, element) {
-  return '实现练习要求，并改写该行代码。';
+  for(var i = 0;i<collection.length;i++){
+    if(collection[i] === element){
+      return collection[i];
+    }
+  }
+  // return '实现练习要求，并改写该行代码。';
 }
